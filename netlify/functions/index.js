61a6f5f44d2ec1b464486aefaca447c90e100de2
Bendegreef1 <ben.degreef@gmail.com>
2025-02-13 13:44:06 +0100

    geen speeldata voorlopig

--- netlify/functions/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function (event, context) {
-    const value = process.env.get("GOOGLE_SHEETS_API_KEY");
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}.` }),
-    };
-};
